cdc3799d8d4d772c6084ddc4342b2aa8aac3220a
mustafa.dagtekin <mustafa.dagtekin@outlook.com>
2021-02-02 16:12:45 +0300

    D02_Where_OfType

---
+++ D02_Where_OfType/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D02_Where_OfType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /************************************Where**********************************************************/
+
+            //ÖRNEK DATA
+            List<Kurs> kurslar1 = new List<Kurs>();
+            kurslar1.Add(new Kurs { ID = 1, Konu = "LINQ Konuları5", Sira = 5 });
+            kurslar1.Add(new Kurs { ID = 2, Konu = "LINQ Konuları4", Sira = 4 });
+            kurslar1.Add(new Kurs { ID = 3, Konu = "LINQ Konuları2", Sira = 3 });
+
+            //QUERY YÖNTEMİ
+            var sonuc = from k in kurslar1
+                        where k.Sira > 3
+                        select k;
+
+            foreach (var kurs in sonuc)
+            {
+                Console.WriteLine(kurs.Konu);
+            }
+            Console.ReadKey();
+
+        }
+
+        class Kurs
+        {
+            public int ID { get; set; }
+            public string Konu { get; set; }
+            public int Sira { get; set; }
+        }
+       
+    }
+}
--- LINQExamples/Program.cs
+++ LINQExamples/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LINQExamples
+namespace D01_Temel
 {
     class Program
     {
